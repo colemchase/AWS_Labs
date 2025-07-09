5977c1bdea04eac3149857730d35c892e72c40cf
colemchase <colemchase@gmail.com>
2025-07-08 23:00:44 -0400

    adding appspec for code deploy

--- codedeploy-lab/main.tf
+++ codedeploy-lab/main.tf
@@ -67,6 +67,26 @@ resource "aws_codedeploy_app" "lambda_app" {
   compute_platform = "Lambda"
 }
 
+resource "aws_iam_role" "lambda_exec" {
+  name = "lambda-codedeploy-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect    = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+
 resource "aws_codedeploy_deployment_group" "lambda_group" {
   app_name              = aws_codedeploy_app.lambda_app.name
   deployment_group_name = "codedeploy-lambda-dg"
