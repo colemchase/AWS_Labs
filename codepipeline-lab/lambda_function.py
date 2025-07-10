7eb65ad9ff7a58246a75f05ac3329af1747028ad
colemchase <colemchase@gmail.com>
2025-07-10 09:44:35 -0400

    renaming

--- codepipeline-lab/lambda_function.py
+++
@@ -1,9 +0,0 @@
-import boto3
-
-def lambda_handler(event, context):
-    client = boto3.client('codepipeline')
-    response = client.start_pipeline_execution(name='your-pipeline-name')
-    return {
-        'statusCode': 200,
-        'body': 'Pipeline triggered!'
-    }
