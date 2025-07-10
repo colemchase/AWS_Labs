b4b31ce3403bf7f3e6bf6f851e8d55327aa97373
colemchase <colemchase@gmail.com>
2025-07-10 08:59:29 -0400

    needed to redploy function

--- codepipeline-lab/main.tf
+++ codepipeline-lab/main.tf
@@ -72,7 +72,7 @@ resource "aws_apigatewayv2_route" "route" {
   target = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
 }
  
-
+# please work
 resource "aws_apigatewayv2_stage" "default" {
   api_id      = aws_apigatewayv2_api.api.id
   name        = "$default"
